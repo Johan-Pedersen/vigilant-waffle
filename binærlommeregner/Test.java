0f7b160e3d1f28686c9179c0d31ec94accb7a8f0
Johan-Pedersen <61731157+Johan-Pedersen@users.noreply.github.com>
2020-03-06 06:42:38 +0100

    Add files via upload

---
+++ binærlommeregner/Test.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(BinCalculator.addBinaryNumber(1,111));
+	
+		System.out.println(BinCalculator.subtractBinaryNumber(101 ,  1101));
+		
+		System.out.println(BinCalculator.multiplication(100, 111));
+	}
+
+}
+ 	
